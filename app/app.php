d1f1e5061b6926fb8ee0e37298aa898c5925e590
Bruno Buiret <bruno.buiret@gmail.com>
2015-03-05 20:27:26 +0100

    Itération #12

--- app/app.php
+++ app/app.php
@@ -1,6 +1,7 @@
 <?php
 use Symfony\Component\Debug\ErrorHandler;
 use Symfony\Component\Debug\ExceptionHandler;
+use Symfony\Component\HttpFoundation\Request;
 
 // Register global error and exception handlers
 ErrorHandler::register();
@@ -90,4 +91,13 @@ $app['dao.comment'] = $app->share(function($app) {
     $commentDAO->setArticleDAO($app['dao.article']);
     $commentDAO->setUserDAO($app['dao.user']);
     return $commentDAO;
+});
+
+// Register JSON data decoder for JSON requests
+$app->before(function(Request $request) {
+    if (0 === strpos($request->headers->get('Content-Type'), 'application/json'))
+    {
+        $data = json_decode($request->getContent(), true);
+        $request->request->replace(is_array($data) ? $data : array());
+    }
 });
